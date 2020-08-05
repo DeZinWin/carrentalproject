0036f72a9134baaa1224f010d2cc215e0613314c
DeZinWin <dezin3106@gmail.com>
2020-08-05 11:54:37 +0630

    i added carlisting update files

--- carlisting.php
+++ carlisting.php
@@ -9,7 +9,72 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-
+  <style>
+    .clearfix::after {
+       content: "";
+       clear: both;
+       display: table;
+    }
+  </style>
+  <script>
+     $(document).ready(function(){
+        $("#cartypeId").change(function(){
+            if($("#cartypeId").val()!=""){
+                var cartypeId=this.value;
+            
+            $.get("type.php?cartype_id="+cartypeId, function(data, status){
+              var carList=JSON.parse(data);  
+              $("#cars").empty();
+            carList.forEach(function(val,i){
+            $("#cars").append(
+               '<div class="clearfix" style="background-color:whitesmoke;padding:10px 10px">' 
+               +'<img src=\"images/'+val.Image+'\"class="float-left" width="260" height="200"> '
+               +'<div class="float-left" style="padding-left:15px">'
+               +'<h3>'+val.CarName+'</h3>'
+               +'<p style="padding-top:15px">'+val.SeatNo+'<span style="padding-left:35px">'+val.Model+'</span></p>'
+               +'<a href="" class="btn btn-primary" style="margin-top:18px;width:200px" role="button" >Details</a>'
+               +'</div>'
+               +'</div>'
+               +'<br>'
+            );
+        });
+     });
+            }
+            
+        
+            });
+     });        
+  </script>
+  <?php
+   include 'config.php';
+   if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+   }
+  $sql="SELECT * FROM cartype";
+  $brandarr=array();
+                
+  if ($result = $conn->query($sql)) {
+                    
+   while($row = $result->fetch_array(MYSQLI_ASSOC)) {
+      $brandarr[] = $row;
+   }
+ } 
+ ?>
+ <?php
+   include 'config.php';
+   if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+   }
+  $sql="SELECT * FROM brand";
+  $companyarr=array();
+                
+  if ($result = $conn->query($sql)) {
+                    
+   while($row = $result->fetch_array(MYSQLI_ASSOC)) {
+      $companyarr[] = $row;
+   }
+ } 
+ ?>
 </head>
 <body>
 <div class="text-center" style="margin-bottom:0">
@@ -32,26 +97,73 @@
     <div class="container-fluid">
        <div class="row">
          <div class="col-sm-4 pl-5">
-            <div style="border:1px solid lavenderblush">
+            <div>
                 <p style="font-size:20px;" class="p-1"><i class="fa fa-car" style="color:#ff8000;font-size:25px"></i><span class="pl-2"><b>Find Your Car</b></span></p>
                 <select style="width:200px;" class="bg-light">
-                 <option>H</option>
+                 <option>Select Brand</option>
+                 <?php
+                if(count($companyarr)>0){
+                foreach($companyarr as $c){
+                   echo "<option value='".$c['id']."'>{$c['CompanyBrand']}</option>";
+            }
+        }
+
+        ?>
                 <select>
                 <br><br>
                 <button style="width:200px" class="btn-warning"><i class="fa fa-search"></i>Search Car</button>
             </div>
             <br><br>
-            <div style="border:1px solid lavenderblush">
+            <div>
                <p style="font-size:20px;" class="p-1"><i class="fa fa-car" style="color:#ff8000;font-size:25px"></i><span class="pl-2"><b>Listed Cars</b></span></p>
-               <button style="width:200px;background-color:#ff8000;">Family Cars</button>
-               <button style="width:200px;background-color:#ff8000;">Cars for two peoples</button>
-               <button style="width:200px;background-color:#ff8000;">Express Cars</button>
+               
+               <select id="cartypeId" style="width:200px;" class="bg-light">
+                 <option>Select CarType</option>
+              <?php
+                if(count($brandarr)>0){
+                foreach($brandarr as $a){
+                   echo "<option value='".$a['id']."'>{$a['Categories']}</option>";
+            }
+        }
+
+        ?>
+    </select>
+                
+             
+               <!--<button style="width:200px;background-color:#ff8000;">Cars for two peoples</button>
+               <button style="width:200px;background-color:#ff8000;">Express Cars</button>-->
             </div>
          </div>
          <div class="col-sm-8">
+          <div id="cars">
+             
+          </div>
          </div>
        </div>
     </div>
 </div>
 </body>
 </html>
+<?php
+   /*include 'config.php';
+   if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  $sql="SELECT * FROM cartype";
+  $brandarr=array();
+  // $brandarr[]=array();
+  if ($result = $conn->query($sql)) {
+      
+    while($row = $result->fetch_array(MYSQLI_ASSOC)) {
+            $brandarr[] = $row;
+    }
+    
+  } 
+  $length=count($brandarr);
+  echo $length;
+  foreach($brandarr as $a){
+              
+    echo $a['Categories'];
+    }
+  $conn->close();*/
+?>
